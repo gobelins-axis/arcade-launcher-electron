da06802933e77b38318b6c6347307152f44a6882
Sergiu_Bo <bocanceaserge335566@gmail.com>
2024-10-17 18:42:52 +0200

    :bug: Update window check

--- src/preload.js
+++ src/preload.js
@@ -1,16 +1,32 @@
 const { ipcRenderer } = require('electron');
 
 window.addEventListener('DOMContentLoaded', () => {
-    if (!window.__axis__) {
-        console.error('Could not find Axis instance');
-        ipcRenderer.send('exit');
-    } else {
-        window.__axis__.set_ipc_renderer(ipcRenderer);
-    }
+    let checkInterval = null;
+    const checkDuration = 5000;
+
+    const checkForAxisInstance = () => {
+        if (window.__axis__) {
+            clearInterval(checkInterval);
+            console.log('Found Axis instance');
+
+            window.__axis__.set_ipc_renderer(ipcRenderer);
+            document.documentElement.classList.add('is-axis-machine');
+        }
+    };
+
+    checkInterval = setInterval(() => {
+        checkForAxisInstance();
+    }, 100);
+
+    setTimeout(() => {
+        if (!window.__axis__) {
+            clearInterval(checkInterval);
+            console.error('Could not find Axis instance after 5 seconds');
+            ipcRenderer.send('exit');
+        }
+    }, checkDuration);
 
     ipcRenderer.on('load-complete', (event, data) => {
         window.__axis__history__index = data;
     });
-
-    document.documentElement.classList.add('is-axis-machine');
 });
